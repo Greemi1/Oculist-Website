5bbeac5fbe664c2353eeba902c24f73d6f10be0d
Greemi <geniuszek22@gmail.com>
2022-04-13 21:24:58 +0200

    add aos library

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ if(isset($_POST['submit'])){
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css"/>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -43,18 +44,18 @@ if(isset($_POST['submit'])){
 
     <div class="row align-items-center justify-content-between">
 
-    <a href="#" class="logo">better<span>Eyes</span></a>
+    <a data-aos="zoom-in-left" data-aos-delay="150" href="#" class="logo">better<span>Eyes</span></a>
 
     <nav class="nav">
-        <a href="#home">home</a>
-        <a href="#about">about</a>
-        <a href="#services">services</a>
-        <a href="#doctors">doctors</a>
-        <a href="#reviews">reviews</a>
-        <a href="#contact">contact</a>
+        <a data-aos="zoom-in-left" data-aos-delay="300" href="#home">home</a>
+        <a data-aos="zoom-in-left" data-aos-delay="450" href="#about">about</a>
+        <a data-aos="zoom-in-left" data-aos-delay="600" href="#services">services</a>
+        <a data-aos="zoom-in-left" data-aos-delay="750" href="#doctors">doctors</a>
+        <a data-aos="zoom-in-left" data-aos-delay="900" href="#reviews">reviews</a>
+        <a data-aos="zoom-in-left" data-aos-delay="1150" href="#contact">contact</a>
     </nav>
 
-    <a href="#contact" class="link-btn">make appointment</a>
+    <a data-aos="zoom-in-left" data-aos-delay="1300" href="#contact" class="link-btn">make appointment</a>
 
     <div id="menu-btn" class="fas fa-bars"></div>
 
@@ -75,9 +76,9 @@ if(isset($_POST['submit'])){
 <div class="row min-vh-100 align-items-center">
 
 <div class="content text-center text-md-left">
-    <h3>with us you will see more</h3>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis harum exercitationem expedita illo itaque esse!</p>
-    <a href="#contact" class="link-btn">make appointment</a>
+    <h3 data-aos="fade-up" data-aos-delay="150">with us you will see more</h3>
+    <p data-aos="fade-up" data-aos-delay="300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis harum exercitationem expedita illo itaque esse!</p>
+    <a data-aos="fade-up" data-aos-delay="450" href="#contact" class="link-btn">make appointment</a>
 </div>
 
 </div>
@@ -96,11 +97,11 @@ if(isset($_POST['submit'])){
 
     <div class="row align-items-center">
 
-    <div class="col-md-6 image">
+    <div class="col-md-6 image" data-aos="fade-right" data-aos-delay="300">
         <img src="images/about.png" alt="" class="w-100 mb-5 mb-md-0">
     </div>
 
-    <div class="col-md-6 content">
+    <div class="col-md-6 content" data-aos="fade-left" data-aos-delay="300">
         <span>about us</span>
         <h3>the quality of your eyesight is our priority</h3>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo, vitae repellendus ullam dicta tenetur corporis dignissimos error quis ea modi. Odio repudiandae eligendi maxime nam dolores? Accusantium, eum. Quisquam, itaque?</p>
@@ -123,37 +124,37 @@ if(isset($_POST['submit'])){
 
     <div class="box-container container">
 
-    <div class="box">
+    <div class="box" data-aos="zoom-in-up" data-aos-delay="150">
         <img src="images/icon1.png" alt="">
         <h3>professional lens replacement</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, quis?</p>
     </div>
 
-    <div class="box">
+    <div class="box" data-aos="zoom-in-up" data-aos-delay="300">
         <img src="images/icon2.png" alt="">
         <h3>immediate selection of glasses</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, quis?</p>
     </div>
 
-    <div class="box">
+    <div class="box" data-aos="zoom-in-up" data-aos-delay="450">
         <img src="images/icon3.png" alt="">
         <h3>eye ultrasound</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, quis?</p>
     </div>
 
-    <div class="box">
+    <div class="box" data-aos="zoom-in-up" data-aos-delay="600">
         <img src="images/icon4.png" alt="">
         <h3>anesthesia</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, quis?</p>
     </div>
 
-    <div class="box">
+    <div class="box" data-aos="zoom-in-up" data-aos-delay="750">
         <img src="images/icon5.png" alt="">
         <h3>all eye examinations</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, quis?</p>
     </div>
 
-    <div class="box">
+    <div class="box" data-aos="zoom-in-up" data-aos-delay="900">
         <img src="images/icon6.png" alt="">
         <h3>all kinds of operations</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, quis?</p>
@@ -173,7 +174,7 @@ if(isset($_POST['submit'])){
 
     <div class="box-container container">
 
-    <div class="box">
+    <div class="box" data-aos="fade-up" data-aos-delay="150">
         <img src="images/doctor1.jpg" alt="">
         <span>specialist</span>
         <h3>john deo</h3>
@@ -185,7 +186,7 @@ if(isset($_POST['submit'])){
         </div>
     </div>
 
-    <div class="box">
+    <div class="box" data-aos="fade-up" data-aos-delay="300">
         <img src="images/doctor2.jpg" alt="">
         <span>specialist</span>
         <h3>john deo</h3>
@@ -197,7 +198,7 @@ if(isset($_POST['submit'])){
         </div>
     </div>
 
-    <div class="box">
+    <div class="box" data-aos="fade-up" data-aos-delay="450">
         <img src="images/doctor3.jpg" alt="">
         <span>specialist</span>
         <h3>john deo</h3>
@@ -209,7 +210,7 @@ if(isset($_POST['submit'])){
         </div>
     </div>
 
-     <div class="box">
+     <div class="box" data-aos="fade-up" data-aos-delay="600">
         <img src="images/doctor4.jpg" alt="">
         <span>specialist</span>
         <h3>john deo</h3>
@@ -221,7 +222,7 @@ if(isset($_POST['submit'])){
         </div>
     </div>
 
-    <div class="box">
+    <div class="box" data-aos="fade-up" data-aos-delay="750">
         <img src="images/doctor5.jpg" alt="">
         <span>specialist</span>
         <h3>john deo</h3>
@@ -233,7 +234,7 @@ if(isset($_POST['submit'])){
         </div>
     </div>
 
-    <div class="box">
+    <div class="box" data-aos="fade-up" data-aos-delay="900">
         <img src="images/doctor6.png" alt="">
         <span>specialist</span>
         <h3>john deo</h3>
@@ -257,7 +258,7 @@ if(isset($_POST['submit'])){
 
     <h1 class="heading">satisfied clients</h1>
 
-    <div class="box-container container swiper reviews-slider">
+    <div class="box-container container swiper reviews-slider" data-aos="fade-up" data-aos-delay="150">
 
     <div class="swiper-wrapper">
 
@@ -361,7 +362,7 @@ if(isset($_POST['submit'])){
 
 <h1 class="heading">make appointment</h1>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" data-aos="fade-down" data-aos-delay="150">
    <?php 
       if(isset($message)){
          foreach($message as $message){
@@ -390,28 +391,28 @@ if(isset($_POST['submit'])){
 
       <div class="box-container container">
 
-      <div class="box">
+      <div class="box" data-aos="fade-up" data-aos-delay="150">
           <i class="fas fa-phone"></i>
           <h3>phone number</h3>
           <p>123-456-789</p>
           <p>111-222-333</p>
       </div>
 
-      <div class="box">
+      <div class="box" data-aos="fade-up" data-aos-delay="300">
           <i class="fas fa-envelope"></i>
           <h3>email address</h3>
           <p>random123@wp.pl</p>
         <p>random123@wp.pl</p>
       </div>
 
-      <div class="box">
+      <div class="box" data-aos="fade-up" data-aos-delay="450">
           <i class="fas fa-clock"></i>
           <h3>opening hours</h3>
                 <p> <span>monday - friday :</span> 10:00 am to 09:00 pm </p>
                 <p> <span>sunday :</span> closed </p>
       </div>
 
-      <div class="box">
+      <div class="box" data-aos="fade-up" data-aos-delay="600">
           <i class="fas fa-map-marker-alt"></i>
           <h3>our address</h3>
           <p>Warsaw, Poland</p>
@@ -428,7 +429,16 @@ if(isset($_POST['submit'])){
 
 
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
 
+<script>
+
+      AOS.init({
+          duration: 800,
+          offset: 150,
+      });
+    
+</script>
 
 <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
 

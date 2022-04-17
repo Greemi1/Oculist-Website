a5bbe8dcb2174ce253bdde173cd751089e492dcf
Greemi <geniuszek22@gmail.com>
2022-04-17 21:53:27 +0200

    remake functions

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const menu = document.querySelector('#menu-btn');
 const navbar = document.querySelector('.header .nav');
 const header = document.querySelector('.header');
 
-menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+menu.addEventListener('click', function(event){
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+});
 
-window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+window.addEventListener('scroll', function(event){
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
 
-    if(window.scrollY > 0){
-        header.classList.add('active');
-    } else {
-        header.classList.remove('active');
-    }
-}
+  if(window.scrollY > 0){
+      header.classList.add('active');
+  } else {
+      header.classList.remove('active');
+  }
+});
 
 const swiper = new Swiper(".reviews-slider", {
     loop: true,
